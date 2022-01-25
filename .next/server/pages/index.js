b1b6617030547db49c3f6e2252b8a49df965dd72
Radymilla Cristiano <radymillacristiano534@gmail.com>
2022-01-24 21:50:34 -0300

    day one - update

---
+++ .next/server/pages/index.js
@@ -0,0 +1,56 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-jsx/style */ \"styled-jsx/style\");\n/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(styled_jsx_style__WEBPACK_IMPORTED_MODULE_1__);\n\n\nfunction HomePage() {\n    //esctiro em jsx\n    return(/*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        className: \"jsx-bffb04f383112cb1\",\n        children: [\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n                className: \"jsx-bffb04f383112cb1\",\n                /*#__PURE__*/ children: \"Boas Vindas\"\n            }, void 0, false, {\n                fileName: \"C:\\\\Users\\\\Cliente\\\\Documents\\\\Visual studio\\\\Aluracord\\\\pages\\\\index.js\",\n                lineNumber: 5,\n                columnNumber: 5\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h2\", {\n                className: \"jsx-bffb04f383112cb1\",\n                children: \"Discord - Alura Matrix\"\n            }, void 0, false, {\n                fileName: \"C:\\\\Users\\\\Cliente\\\\Documents\\\\Visual studio\\\\Aluracord\\\\pages\\\\index.js\",\n                lineNumber: 6,\n                columnNumber: 5\n            }, this),\n            (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)((styled_jsx_style__WEBPACK_IMPORTED_MODULE_1___default()), {\n                id: \"bffb04f383112cb1\",\n                children: \"h1.jsx-bffb04f383112cb1{color:red}\"\n            }, void 0, false, void 0, this)\n        ]\n    }, void 0, true, {\n        fileName: \"C:\\\\Users\\\\Cliente\\\\Documents\\\\Visual studio\\\\Aluracord\\\\pages\\\\index.js\",\n        lineNumber: 4,\n        columnNumber: 3\n    }, this));\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (HomePage);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O1NBQVNBLFFBQVEsR0FBRyxDQUFDO0lBQ25CLEVBQWdCO0lBQ2hCLE1BQU0sNkVBQ0xDLENBQUc7Ozt3RkFDREMsQ0FBRTs7d0NBQUMsQ0FBVzs7Ozs7O3dGQUNkQyxDQUFFOzswQkFBQyxDQUFzQjs7Ozs7Ozs7Ozs7Ozs7OztBQVE5QixDQUFDO0FBRUQsaUVBQWVILFFBQVEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9BbHVyYWNvcmQvLi9wYWdlcy9pbmRleC5qcz9iZWU3Il0sInNvdXJjZXNDb250ZW50IjpbImZ1bmN0aW9uIEhvbWVQYWdlKCkge1xyXG4gIC8vZXNjdGlybyBlbSBqc3hcclxuICByZXR1cm4gKFxyXG4gIDxkaXY+XHJcbiAgICA8aDE+Qm9hcyBWaW5kYXM8L2gxPlxyXG4gICAgPGgyPkRpc2NvcmQgLSBBbHVyYSBNYXRyaXg8L2gyPlxyXG4gICAgPHN0eWxlIGpzeD57YFxyXG4gICAgICBoMSB7XHJcbiAgICAgICAgY29sb3I6IHJlZDtcclxuICAgICAgfVxyXG4gICAgICBgfTwvc3R5bGU+XHJcbiAgPC9kaXY+XHJcbiAgKVxyXG59XHJcblxyXG5leHBvcnQgZGVmYXVsdCBIb21lUGFnZSJdLCJuYW1lcyI6WyJIb21lUGFnZSIsImRpdiIsImgxIiwiaDIiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ }),
+
+/***/ "styled-jsx/style":
+/*!***********************************!*\
+  !*** external "styled-jsx/style" ***!
+  \***********************************/
+/***/ ((module) => {
+
+module.exports = require("styled-jsx/style");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+function HomePage() {
+  //esctiro em jsx
+  return (
+  <div>
+    <h1>Boas Vindas</h1>
+    <h2>Discord - Alura Matrix</h2>
+    <style jsx>{`
+      h1 {
+        color: red;
+      }
+      `}</style>
+  </div>
+  )
+}
+
+export default HomePage
